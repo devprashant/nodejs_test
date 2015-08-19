e23f7eb604a5fb787d1a1b82586eb0d503581b2b
cstay <prashantdawar.2008@gmail.com>
2015-08-19 05:19:46 +0000

    managing child processes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//working with events
-var BookClass = require("/books.js");
-var book = new BookClass();
-book.on('rated', function() {
-    console.log('Rated with ' + book.getPoints());
+//Managin child processes
+
+var exec = require("child_process").exec;
+exec('ls -l', function(error, stdout, stderr){
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null){
+        console.log('exec error: ' + error);
+    }
 });
-book.rate(10);
