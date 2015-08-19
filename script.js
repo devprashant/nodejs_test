8e2337cf059ceb1647a388fd52c49da00dfdc42e
cstay <prashantdawar.2008@gmail.com>
2015-08-19 06:12:45 +0000

    created server base

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //Architecting the Project
 var http = require("http");
-http.createServer(function (req, res) {
-   res.writeHead(200, { 'Content-Type' : "text/plain"}) ;
-   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var fs = require("fs");
+http.createServer(function (req, res){
+   var content = "";
+   var type = '';
+   if (req.url === '/'){
+       content = fs.readFileSync('web/page.html');
+       type = "text/html";
+   } else if (req.url === 'web/style.css'){
+       content = fs.readFileSync('web/style.css');
+       type = "text/css";
+   }
+   res.writeHead(200, { 'Content-Type' : type});
+   res.end(content + '\n');
+}).listen(1337, 'https://nodejst-prashantdawar.c9.io');
+
 console.log('Server running at http://127.0.0.1:1337/');
