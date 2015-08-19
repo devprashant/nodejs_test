c9755ac7fa0bd67da229e90abafa6523921c6327
cstay <prashantdawar.2008@gmail.com>
2015-08-19 05:11:22 +0000

    events with errors

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //working with events
-var events = require("events");
-var eventEmitter = new events.EventEmitter();
-var somethingHappen = function() {
-    console.log('Something happen!');
-}
-eventEmitter
-.on('something-happen', somethingHappen)
-.emit('something-happen');
+var BookClass = require("/books.js");
+var book = new BookClass();
+book.on('rated', function() {
+    console.log('Rated with ' + book.getPoints());
+});
+book.rate(10);
