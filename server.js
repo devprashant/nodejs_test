3d2281f348bd0886a55b0f7243da456d9edc8463
devprashant <devprashantdawar@gmail.com>
2015-08-20 06:49:15 +0000

    building server for restful api

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
-var http = require("http"), res;
-var controller = function(request, response){
-    res = response;
-    if (request.url === '/on'){
-        model.update(true);
-    } else if (request.url === '/off'){
-        model.update(false);
-    } else {
-        view.render();
+var http = require("http");
+var url = require("url");
+var controller = function(req, res) {
+    var message = '';
+    switch(req.method){
+        case 'GET': message = "Thats GET message"; break;
+        case 'POST': message = "That's POST message"; break;
+        case 'PUT': message = "That's PUT message"; break;
+        case 'DELETE' : message = "That's DELETE message"; break;
     }
-};
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(message + "\n");
+}
 http.createServer(controller).listen(process.env.PORT, process.env.IP);
-console.log('Server running at http://'+process.env.IP+":"+process.env.PORT);
-
-
-var view = {
-    render: function() {
-        var html = '';
-        html += '<!DOCTYPE html>';
-        html += '<html>';
-        html += '<head><title>Node.js byexample</title></head>';
-        html += '<body>';
-        html += '<h1>Status ' + (model.status ? 'on' : 'off') + '</h1>';
-        html += '<a href="/on">switch on</a><br />';
-        html += '<a href="/off">switch off</a><br />';
-        html += '</body>';
-        html += '</html>';
-        res.writeHead(200, { 'Content-Type' : 'text/html'});
-        res.end(html + '\n');
-    }
-};
-
-var model = {
-    status: false,
-    update: function(s) {
-        this.status = s;
-        view.render();
-    }
-};
+console.log('Server running at http://'+ process.env.IP + ":"+process.env.PORT);
