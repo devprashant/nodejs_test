10c92a39b15a6ee583f294610723ea1bf9e71736
devprashant <devprashantdawar@gmail.com>
2015-08-20 09:32:42 +0000

    Serving files with node.js

--- server.js
+++ server.js
@@ -1,33 +1,58 @@
 var http = require("http");
-var url = require("url");
-var controller = function(req, res) {
-    var message = '';
-    switch(req.method){
-        case 'GET': message = "Thats GET message"; break;
-        case 'POST': message = "That's POST message"; break;
-        case 'PUT': 
-            processRequest(req, function(data){
-               message = "That's PUT message. You are editing " + data.book + " book." ;
-               res.writeHead(200, {'Content-Type': 'text/html'});
-               res.end(message + "\n");
-            });
-            return;
-        break;
-        case 'DELETE' : message = "That's DELETE message"; break;
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(message + "\n");
-};
-http.createServer(controller).listen(process.env.PORT, process.env.IP);
-console.log('Server running at http://'+ process.env.IP + ":"+process.env.PORT);
+var fs = require("fs");
+var path = require("path");
 
-var qs = require('querystring');
-var processRequest = function(req, callback) {
-    var body = '';
-    req.on('data', function (data) {
-        body += data;
-    });
-    req.on('end', function() {
-        callback(qs.parse(body));
-    });
+var files = {};
+var port = process.env.PORT;
+var host = process.env.IP;
+
+var assets = function(req, res){
+var sendError = function(message, code){
+    if(code === undefined)  {
+        code = 404;
+    }
+    res.writeHead(code, {'Content-Type' : 'text/html'});
+    res.end(message);
+};
+
+var serve = function(file){
+    var contentType;
+    switch(file.ext.toLowerCase()){
+        case "css": contentType = "text/css"; break;
+        case "html": contentType = "text/html"; break;
+        case "js": contentType = "application/javascript"; break;
+        case "ico": contentType = "application.ico"; break;
+        case "json": contentType = "application/json"; break;
+        case "jpg": contentType = "image/jpeg"; break;
+        case "jpeg": contentType = "image/jpeg"; break;
+        case "png": contentType = "image/png"; break;
+        default: contentType = "text/plain";
+    }
+    res.writeHead(200, {'Content-Type': contentType});
+    res.end(file.content);
+};
+
+var readFile = function(filePath){
+    if (files[filePath]){
+        serve(files[filePath]);
+    } else {
+        fs.readFile(filePath, function(err, data){
+            if(err){
+                sendError('Error reading ' + filePath + '.');
+                return;
+            }
+            files[filePath] = {
+                ext: filePath.split(".").pop(),
+                content: data
+            };
+            serve(files[filePath]);
+        });
+    }
 }
+
+readFile(path.normalize(__dirname + req.url));
+};
+
+
+var app = http.createServer(assets).listen(port,host);
+console.log("Listening on " + host + ":" + port);
